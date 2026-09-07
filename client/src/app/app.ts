38f6d35732ac0354668571341af1a7799ce81228
Gabriel Guerra <Gabrielguerrait@hotmail.com>
2026-09-07 16:16:26 +0200

    config init

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+
+  private http= inject(HttpClient);
+  protected readonly title =  'Dating App'; 
+
+  ngOnInit(): void {
+
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+  }
+  
 }
