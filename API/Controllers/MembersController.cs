58493771023026b9807c04a17aa0b9ce52eef465
Gabriel Guerra <Gabrielguerrait@hotmail.com>
2026-09-07 14:44:22 +0200

    aggiunto GetMember e GetMembers

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController(AppDbContext context) : Controller
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await context.User.ToListAsync();
+        return Ok(members);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.User.FindAsync(id);
+        if (member == null) return NotFound();
+        return Ok(member);
+    }
+}
+
--- API/Program.cs
+++ API/Program.cs
@@ -4,13 +4,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
 
 var app = builder.Build();
 
