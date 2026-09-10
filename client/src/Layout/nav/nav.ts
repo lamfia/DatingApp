5ab72e7a3fe650c23cbe82ae621514590182a284
Gabriel Guerra <Gabrielguerrait@hotmail.com>
2026-09-10 15:52:46 +0200

    inizio persistent login in local storage

--- client/src/Layout/nav/nav.ts
+++ client/src/Layout/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
@@ -9,11 +10,30 @@ import { FormsModule } from '@angular/forms';
 })
 export class Nav {
 
+  private accountService = inject(AccountService)
   protected creds: any = {}
+  protected loggedIn = signal(false)
 
   login() {
-    console.log(this.creds)
+
+    this.accountService.login(this.creds).subscribe({
+      next: (response) => {
+        console.log(response)
+        this.loggedIn.set(true)
+        this.creds = {}
+      },
+      error: (error) => {
+        alert(error)
+      },
+      complete: () => {
+        console.log("Login request completed.")
+      }
+    })
+
   }
 
+  logout() {
+    this.loggedIn.set(false)
+  }
 
 }
